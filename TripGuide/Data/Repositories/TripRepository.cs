a5ddb99c7872a1a57ffd07a78fcaaa4233ed3ee5
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2023-08-29 21:13:17 +0200

    Finish trip repository

--- TripGuide/Data/Repositories/TripRepository.cs
+++ TripGuide/Data/Repositories/TripRepository.cs
@@ -15,8 +15,8 @@ public class TripRepository
     public async Task<List<Trip>> GetAllByUserIdAsync(string userId)
     {
         return await _applicationDbContext.Trips
-            .Include(x => x.PlacesToVisit)
-            .Where(y => y.UserId == userId)
+            .Include(t => t.PlacesToVisit)
+            .Where(t => t.UserId == userId)
             .ToListAsync();
     }
 
@@ -25,5 +25,79 @@ public class TripRepository
         return await _applicationDbContext.Trips
             .Include(t => t.PlacesToVisit)
             .SingleOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task CreateTripAsync(string userId, string name, string location, DateTime startDate, DateTime endDate)
+    {
+        var newTrip = new Trip
+        {
+            UserId = userId,
+            Name = name,
+            Location = location,
+            StartDate = startDate,
+            EndDate = endDate
+        };
+
+        _applicationDbContext.Add(newTrip);
+        await _applicationDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteTripAsync(int id)
+    {
+        var tripToDeletion = _applicationDbContext.Trips
+            .Include(t => t.PlacesToVisit)
+            .SingleOrDefault(t => t.Id == id);
+
+        if (tripToDeletion == null)
+        {
+            return;
+        }
+        
+        _applicationDbContext.PlacesToVisit.RemoveRange(tripToDeletion.PlacesToVisit);
+        
+        _applicationDbContext.Trips.Remove(tripToDeletion);
+        await _applicationDbContext.SaveChangesAsync();
+    }
+
+    public async Task CreatePlaceToVisit(
+        int tripId, 
+        string name, 
+        int visitOrder, 
+        string note, 
+        int longitude, 
+        int latitude)
+    {
+        var trip = await _applicationDbContext.Trips.SingleOrDefaultAsync(t => t.Id == tripId);
+        
+        if (trip == null)
+        {
+            return;
+        }
+
+        var newPlace = new PlaceToVisit
+        {
+            TripId = tripId,
+            Name = name,
+            VisitOrder = visitOrder,
+            Note = note,
+            Longitude = longitude,
+            Latitude = latitude
+        };
+        _applicationDbContext.PlacesToVisit.Add(newPlace);
+        await _applicationDbContext.SaveChangesAsync();
+    }
+
+    public async Task DeletePlaceToVisitAsync(int id)
+    {
+        var placeToDelete = await _applicationDbContext.PlacesToVisit
+            .SingleOrDefaultAsync((p => p.Id == id));
+
+        if (placeToDelete == null)
+        {
+            return;
+        }
+
+        _applicationDbContext.PlacesToVisit.Remove(placeToDelete);
+        await _applicationDbContext.SaveChangesAsync();
     }
 }
